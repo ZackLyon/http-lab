3a0200bea7912068a81bd0052c812716eb822b3a
Zack Lyon <zack.j.lyon@gmail.com>
2021-10-29 10:09:04 -0700

    Passing POST /echo tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -15,9 +15,35 @@ describe('app routes', () => {
     const response = await fakeRequest(app).get('/');
     const expected = 'text/plain';
 
-    // const parsedRequest = parseRequest(response);
     const contentType = response.header['content-type'];
 
     expect(contentType).toEqual(expected);
+  });
+
+  it('should return whatever is in the body of the request for POST on /echo path', async () => {
+    const response = await fakeRequest(app).post('/echo').send('echoECHOecho');
+
+    const expected = 'echoECHOecho';
+
+    expect(response.text).toEqual(expected);
+  });
+
+  it('should return content type text/plain for POST on /echo path', async () => {
+    const response = await fakeRequest(app).post('/echo').send('echoECHOecho');
+
+    const expected = 'text/plain';
+
+    const contentType = response.header['content-type'];
+
+    expect(contentType).toEqual(expected);
+  });
+
+  it('should return status 200 for POST on /echo path', async () => {
+    const response = await fakeRequest(app).post('/echo').send('echoECHOecho');
+    const expected = 200;
+
+    const status = response.statusCode;
+
+    expect(status).toEqual(expected);
   });
 });
