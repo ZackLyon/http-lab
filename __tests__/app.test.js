2138d90c361e955185d07e9f9ce4eb2e499fcc1f
Zack Lyon <zack.j.lyon@gmail.com>
2021-10-29 09:54:21 -0700

    tests for GET / route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,23 @@
 const request = require('supertest');
 const app = require('../lib/app');
+const fakeRequest = require('supertest');
+const parseRequest = require('../lib/utils/parseRequest.js');
 
 describe('app routes', () => {
+  it('should return hi for GET request on / path', async () => {
+    const response = await fakeRequest(app).get('/');
+    const expected = 'hi';
 
+    expect(response.text).toEqual(expected);
+  });
+
+  it('should return content type text/plain for GET request on / path', async () => {
+    const response = await fakeRequest(app).get('/');
+    const expected = 'text/plain';
+
+    // const parsedRequest = parseRequest(response);
+    const contentType = response.header['content-type'];
+
+    expect(contentType).toEqual(expected);
+  });
 });
