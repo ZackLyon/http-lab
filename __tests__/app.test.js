5231de03e7a423760f3b270d210dd36894f179ac
Zack Lyon <zack.j.lyon@gmail.com>
2021-10-29 10:55:21 -0700

    Passing color route tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -46,4 +46,37 @@ describe('app routes', () => {
 
     expect(status).toEqual(expected);
   });
+
+  it('should return <h1>red</h1> for GET on /red path', async () => {
+    const response = await fakeRequest(app).get('/red');
+    const expected = '<h1>red</h1>';
+
+    expect(response.text).toEqual(expected);
+  });
+
+  it('should return <h1>blue</h1> for GET on /blue path', async () => {
+    const response = await fakeRequest(app).get('/blue');
+    const expected = '<h1>blue</h1>';
+
+    expect(response.text).toEqual(expected);
+  });
+
+  it('should return <h1>green</h1> for GET on /green path', async () => {
+    const response = await fakeRequest(app).get('/green');
+    const expected = '<h1>green</h1>';
+
+    expect(response.text).toEqual(expected);
+  });
+
+  it('should return content type text/html for GET on /red, /green, or /blue path', async () => {
+    const response = await fakeRequest(app).get('/red');
+
+    const expected = 'text/html';
+
+    const contentType = response.header['content-type'];
+
+    expect(contentType).toEqual(expected);
+  });
+
+  //must write html page for not found
 });
